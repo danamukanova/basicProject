c7a0c90ad026cf167011d2c30324df0facafaf56
danamukanova <d.mukanova@petrel.ai>
2022-04-25 19:08:49 +0600

    load basic project

---
+++ 02-tours/setup/src/Tour.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Tour = ({ id, image, info, price, name }) => {
+    return ( < article className = 'single-tour' >
+        <
+        img src = { image }
+        alt = { name }
+        /> <
+        footer > < div className = 'tour-info ' > < h4 > { name } < /h4>
+
+        <
+        h4 className = 'totalPrice' > { price } < /h4>< /div > < p > { info } < /p>< /footer > < /
+        article > );
+};
+
+export default Tour;
